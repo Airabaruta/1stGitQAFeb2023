853e09c58b6d6d3fae41351c4fb789dac3c49dac
Airabaruta <irabarutahappy1@gmail.com>
2023-07-14 02:28:02 -0500

    modified

--- src/test/java/homework01/GitTest11.java
+++ src/test/java/homework01/GitTest11.java
@@ -1,5 +1,17 @@
 package homework01;
 
 public class GitTest11 {
-
+	
+	public static void main(String[] args) {
+		System.out.println("git test");
+	    
+	}
+    public static void printMe() {
+    	
+    	System.out.println("git test 1");
+    	System.out.println("git test 2");
+    	System.out.println("git test 3.1");
+    	System.out.println("git test 4.4");
+    	System.out.println("git test 5");
+    }
 }
